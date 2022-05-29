6baceab44213daf4dc78b3875e2fe75ab9f05cfd
quyenhp15 <quyenhp15@gmail.com>
2022-05-30 01:17:52 +0700

    connect database success

--- frontend/src/components/Register/Registration.jsx
+++ frontend/src/components/Register/Registration.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import './Registration.css'
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 const Registration = () => {
 
@@ -14,30 +15,55 @@ const Registration = () => {
   //   }
   // }
 
+  const usernameRef = useRef(null)
+  const fullNameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+
   const navigate = useNavigate()
 
   const signIn = () => {
-
-    // window.location.href = "/login";
-    navigate('/login')
+    window.location.href = "/login";
+    // navigate('/login')
   };
+
+  const buttonSignUp = (event) => {
+    event.preventDefault()
+
+    const registered = {
+      "fullName": fullNameRef.current.value,
+      "username": usernameRef.current.value,
+      "password": passwordRef.current.value,
+      "email": emailRef.current.value
+    }
+
+    axios.post('http://localhost:4000/app/signup', registered).then(response => console.log(response.data))
+
+    console.log(fullNameRef.current.value)
+    console.log(usernameRef.current.value)
+    console.log(passwordRef.current.value)
+    console.log(emailRef.current.value)
+
+  }
 
   return (
 
     <div className="register-page" >
       <div className="header-bar">
 
-
       </div>
       <div className="form__sign-up">
         <div className="signIn__title">
           <h1>Sign Up</h1>
         </div>
-        <input placeholder="Username" className="input__user-name" />
-        <input type="password" placeholder="Password" className="input__password" />
+        <input ref={fullNameRef} />
+        <input ref={emailRef} />
+
+        <input placeholder="Username" className="input__user-name" ref={usernameRef} />
+        <input type="password" placeholder="Password" className="input__password" ref={passwordRef} />
         <input type="password" placeholder="Confirm password" className="input__password" />
 
-        <div className="btn_sign-in">Sign Up</div>
+        <button className="btn_sign-in" onClick={buttonSignUp}>Sign Up</button>
 
 
 
