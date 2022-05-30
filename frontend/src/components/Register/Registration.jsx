512648e01a7aedd1d92ef2be74168df5ac613987
quyenhp15 <quyenhp15@gmail.com>
2022-05-31 00:43:35 +0700

    register

--- frontend/src/components/Register/Registration.jsx
+++ frontend/src/components/Register/Registration.jsx
@@ -5,10 +5,12 @@ import axios from "axios";
 
 import 'antd/dist/antd.css'
 import { Input, DatePicker, Button, message, Form } from 'antd';
+import { useForm } from "antd/lib/form/Form";
 
 const Registration = () => {
 
   const navigate = useNavigate()
+  // const { reset } = useForm();
 
   const signIn = () => {
     window.location.href = "/login";
@@ -16,27 +18,30 @@ const Registration = () => {
   };
 
   const buttonSignUp = (event) => {
-    event.preventDefault()
+    // event.preventDefault()
+
+    if (event.password !== event.confirmPassword) {
+      message.error('Password and Confirm password does not the same')
+      // reset()
+      return;
+    }
 
     const registered = {
-      // "studentID": studentIDRef.current.value,
-      // "studentName": studentNameRef.current.value,
-      // "dateOfBirth": dateOfBirthRef.current.value,
-      // "password": passwordRef.current.value,
-      // "email": emailRef.current.value
+      "studentID": event.studentID,
+      "studentName": event.studentName,
+      "dateOfBirth": event.birthday,
+      "password": event.password,
+      "email": event.email
     }
 
     axios.post('http://localhost:4000/LibSystem/signup', registered).then(response => console.log(response.data))
+    message.success('Sign Up success')
 
   }
 
   const onchange = (e) => {
     console.log(e.target.value)
   }
-
-  const onFinish = (values) => {
-    console.log('Success:', values.username);
-  };
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
@@ -55,54 +60,40 @@ const Registration = () => {
         </div>
 
         <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }}
-          onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off" >
-          <Form.Item name="studentID" rules={[{
-            required: true,
-            message: 'Please input student ID!',
-          },]}          >
+          onFinish={buttonSignUp} onFinishFailed={onFinishFailed} autoComplete="off" >
+          <Form.Item name="studentID" rules={[{ required: true, message: 'Do not empty', },]}          >
             <Input placeholder="Student ID" className="inputInfo" />
           </Form.Item>
 
-          <Form.Item name="username" rules={[{
-            required: true,
-            message: 'Please input your username!',
-          },]}          >
-            <Input placeholder="Student ID" className="inputInfo" />
+          <Form.Item name="studentName" rules={[{ required: true, message: 'Do not empty', },]}          >
+            <Input placeholder="Student name" className="inputInfo" />
           </Form.Item>
 
-          <Form.Item label="Password" name="password" rules={[{
-            required: true,
-            message: 'Please input your password!',
-          },]}          >
-            <Input.Password />
+          <Form.Item name="birthday" rules={[{ required: true, message: 'Do not empty', },]}          >
+            <DatePicker placeholder="Birthday" className="inputInfo" />
           </Form.Item>
 
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
+          <Form.Item name="email" rules={[{ required: true, message: 'Do not empty', },]}          >
+            <Input placeholder="Email" className="inputInfo" />
+          </Form.Item>
+
+          <Form.Item name="password" rules={[{ required: true, message: 'Do not empty', },]}          >
+            <Input.Password placeholder="Password" className="inputInfo" />
+          </Form.Item>
+
+          <Form.Item name="confirmPassword" rules={[{ required: true, message: 'Do not empty', },]}          >
+            <Input.Password placeholder="Confirm password" className="inputInfo" />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16, }}          >
+            <Button type="primary" htmlType="submit" className="btn_sign-up">
+              Sign Up
             </Button>
           </Form.Item>
 
         </Form>
 
-        <input placeholder="Student ID" className="inputInfo" />
-        <input placeholder="Student name" className="inputInfo" />
-        <DatePicker placeholder="Birthday" className="inputInfo" />
-        <br />
-        <br />
-        <input placeholder="Email" className="inputInfo" />
-
-        <Input.Password type="password" placeholder="Password" className="input__password" onChange={onchange} />
-        <Input.Password type="password" placeholder="Confirm password" className="input__password" />
-
-        <button className="btn_sign-in" onClick={buttonSignUp}>Sign Up</button>
-
-
+        {/* <button className="btn_sign-in" onClick={buttonSignUp}>Sign Up</button> */}
 
         <p style={{ marginTop: "6%", marginLeft: "40%", cursor: "pointer" }} onClick={signIn}>
           Sign Up Now?
