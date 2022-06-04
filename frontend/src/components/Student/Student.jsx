516ced1e8133e6794244e858be6f0e3f076bea2c
quyenhp15 <quyenhp15@gmail.com>
2022-06-05 00:55:53 +0700

    student page

---
+++ frontend/src/components/Student/Student.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import './Admin.css'
+import axios from "axios";
+import 'antd/dist/antd.css'
+
+const Student = () => {
+
+    return (
+        <div>
+            Student page
+        </div>
+    )
+}
+
+export default Student
